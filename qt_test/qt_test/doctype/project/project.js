fdb65f8dc90d7fd98502fe266b9b828e13d140ac
Jean Francois Munyaneza <jeanfrancoismunyaneza@Jeans-MacBook-Pro.local>
2024-05-08 11:35:40 +0200

    feat: add date validation on both project and task forms

--- qt_test/qt_test/doctype/project/project.js
+++ qt_test/qt_test/doctype/project/project.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Project', {
-	// refresh: function(frm) {
-
-	// }
+  start_date: function (frm) {
+    // Validate start date
+    if (frm.doc.start_date && frm.doc.end_date) {
+      if (frm.doc.start_date > frm.doc.end_date) {
+        frm.set_value('start_date', null);
+        frappe.throw(__('Start date can not be greater than the End date'));
+      }
+    }
+  },
+  end_date: function (frm) {
+    // Validate end date
+    if (frm.doc.start_date && frm.doc.end_date) {
+      if (frm.doc.end_date < frm.doc.start_date) {
+        frm.set_value('end_date', null);
+        frappe.throw(__('End date can not be less than the Start date'));
+      }
+    }
+  },
 });
