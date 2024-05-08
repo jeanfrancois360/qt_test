2cfb502cd01e809586e981c946629918d0222d9f
Jean Francois Munyaneza <jeanfrancoismunyaneza@Jeans-MacBook-Pro.local>
2024-05-08 10:59:19 +0200

    feat: add task management module

---
+++ qt_test/qt_test/doctype/tasks/tasks.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Jean Francois Munyaneza and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Tasks', {
+	// refresh: function(frm) {
+
+	// }
+});
